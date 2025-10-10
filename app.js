3b8691cbc64f532554e8bf1721cbee2bb8c5e065
lnitc <nazebesto@yahoo.com>
2025-10-11 07:59:36 +0900

    Route refactoring

--- app.js
+++ app.js
@@ -8,13 +8,7 @@ function findTour(req, tours) {
   return tours.find((el) => el.id === id);
 }
 
-app.use(express.json()); //middleware to be able to use req.body
-
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
-);
-
-app.get('/api/v1/tours', (req, res) => {
+function getAllTours(req, res) {
   res.status(200).json({
     status: 'success',
     results: tours.length,
@@ -22,33 +16,12 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
-});
+}
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  const tour = findTour(req, tours);
-
-  if (!tour) {
-    return res.status(404).json({
-      status: 'fail',
-      message: 'Invalid ID',
-    });
-  }
-
-  res.status(200).json({
-    status: 'success',
-    data: {
-      tour,
-    },
-  });
-});
-
-app.post('/api/v1/tours', (req, res) => {
-  //console.log(req.body);
-
+function createTour(req, res) {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
   tours.push(newTour);
-  //need to use async code inside the callback fn in order not to block the event loop
   fs.writeFile(
     `${__dirname}/dev-data/data/tours-simple.json`,
     JSON.stringify(tours),
@@ -62,41 +35,67 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+}
 
-app.patch('/api/v1/tours/:id', (req, res) => {
+function getTour(req, res) {
   const tour = findTour(req, tours);
-
   if (!tour) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
   }
-
   res.status(200).json({
     status: 'success',
     data: {
-      tour: '<Updated tour here...>',
+      tour,
     },
   });
-});
+}
 
-app.delete('/api/v1/tours/:id', (req, res) => {
+function updateTour(req, res) {
   const tour = findTour(req, tours);
-
   if (!tour) {
     return res.status(404).json({
       status: 'fail',
       message: 'Invalid ID',
     });
   }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: req.body,
+    },
+  });
+}
 
+function deleteTour(req, res) {
+  const tour = findTour(req, tours);
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid ID',
+    });
+  }
   res.status(204).json({
     status: 'success',
     data: null,
   });
-});
+}
+
+app.use(express.json()); //middleware to be able to use req.body
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+//route allows method chaining
+app.route('/api/v1/tours').get(getAllTours).post(createTour);
+app
+  .route('/api/v1/tours/:id')
+  .get(getTour)
+  .patch(updateTour)
+  .delete(deleteTour);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}`);

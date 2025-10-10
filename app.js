8b4bf08463ae2f5da8cea2fdce25d4bf07c2e0d6
lnitc <nazebesto@yahoo.com>
2025-10-11 06:00:01 +0900

    API: handle get requests

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 8000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
 
-app.post('/', (req, res) => {
-  res.send('Got a POST request');
+// app.post('/', (req, res) => {
+//   res.send('Got a POST request');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`, 'utf-8')
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
 app.listen(port, () => {

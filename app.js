b89021df130f08ae37e41ca9ba27079aed3835cd
lnitc <nazebesto@yahoo.com>
2025-10-11 06:37:08 +0900

    API: handle post requests

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 8000;
 
+app.use(express.json()); //middleware to be able to use req.body
+
 // app.get('/', (req, res) => {
 //   res
 //     .status(200)
@@ -25,6 +27,28 @@ app.get('/api/v1/tours', (req, res) => {
       tours,
     },
   });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+  tours.push(newTour);
+  //need to use async code inside the callback fn in order not to block the event loop
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        //201 status stands for created
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
 });
 
 app.listen(port, () => {

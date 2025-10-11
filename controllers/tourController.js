27c5224407060db0c27cdd5e687d39df187419f3
lnitc <nazebesto@yahoo.com>
2025-10-12 04:01:09 +0900

    Add logic to create tours

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,43 +1,31 @@
 const fs = require('fs');
 const Tour = require('./../models/tourModel');
 
-function checkBody(req, res, next) {
-  if (!('price' in req.body) || !('name' in req.body)) {
-    return res.status(400).json({
-      status: 'fail',
-      message: 'Missing name or price',
-    });
-  }
-  next();
-}
-
 function getAllTours(req, res) {
   res.status(200).json({
     status: 'success',
-   // results: tours.length,
+    // results: tours.length,
     // data: {
     //   tours,
     // },
   });
 }
 
-function createTour(req, res) {
-  // const newId = tours[tours.length - 1].id + 1;
-  // const newTour = Object.assign({ id: newId }, req.body);
-  // tours.push(newTour);
-  // fs.writeFile(
-  //   `${__dirname}/../dev-data/data/tours-simple.json`,
-  //   JSON.stringify(tours),
-  //   (err) => {
-  //     res.status(201).json({
-  //       //201 status stands for created
-  //       status: 'success',
-  //       data: {
-  //         tour: newTour,
-  //       },
-  //     });
-  //   }
-  // );
+async function createTour(req, res) {
+  try {
+    const newTour = await Tour.create(req.body);
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: 'Invalid data sent',
+    });
+  }
 }
 
 function getTour(req, res) {
@@ -72,5 +60,4 @@ module.exports = {
   getTour,
   updateTour,
   deleteTour,
-  checkBody,
 };

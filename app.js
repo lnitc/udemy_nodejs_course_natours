da6318e8ee0184349ef0145e7100ca723ab422d7
lnitc <nazebesto@yahoo.com>
2025-10-12 00:11:25 +0900

    Configure dotenv, prettier and eslint for the project

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const morgan = require('morgan');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+
 const app = express();
 
 //middleware
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json()); //middleware to be able to use req.body
+app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');

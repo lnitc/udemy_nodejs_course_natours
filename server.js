3f4e82740c42c731e25e51a5a2b14a6e81fe5c52
lnitc <nazebesto@yahoo.com>
2025-10-12 02:42:44 +0900

    Configure mongoDB

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
 
-const port = process.env.PORT || 8000;
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+  })
+  .then((con) => {
+    console.log('DB connection successful!', con.connections);
+  });
 
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
   console.log(`App running on port ${port}`);
 });

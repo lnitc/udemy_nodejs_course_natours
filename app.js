38f37359e952e922ce76fac4197fed5b5649c895
lnitc <nazebesto@yahoo.com>
2025-10-21 13:20:04 +0900

    Add catch-all routes and global error handler

--- app.js
+++ app.js
@@ -24,7 +24,30 @@ app.use((req, res, next) => {
   next();
 });
 
+//routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+//catch all
+app.all('/*path', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Can't find ${req.originalUrl} on this server!`,
+  // });
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`); //custom error
+  err.statusCode = 404;
+  err.status = 'fail';
+  next(err); //the argument passed to next is assumed to be an error and the rest of the middleware is skipped
+});
+
+//global error handler
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;

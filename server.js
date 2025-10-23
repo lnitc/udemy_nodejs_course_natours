0dc9bfe5dcb69eb93ab6d7a966696240a2d56d15
lnitc <nazebesto@yahoo.com>
2025-10-23 11:32:51 +0900

    Handle unhandled rejections and uncaught exceptions

--- server.js
+++ server.js
@@ -1,5 +1,14 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
+//detect uncaught exceptions
+//the server needs to crash because after uncaught exception the whole node environment is compromised
+process.on('uncaughtException', (err) => {
+  console.log('UNCAUGHT EXCEPTION! Shutting down...');
+  console.log(err.name, err.message);
+  process.exit(1); //1 means error
+});
+
 dotenv.config({ path: './config.env' });
 const app = require('./app'); //import app after dotenv to be able to use morgan
 
@@ -10,6 +19,16 @@ mongoose.connect(process.env.DATABASE).then((con) => {
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App running on port ${port}`);
+});
+
+//detect unhandled promise rejections
+process.on('unhandledRejection', (err) => {
+  console.log('UNHANDLED REJECTION! Shutting down...');
+  console.log(err.name, err.message);
+  //give the server time to finish handling current requests
+  server.close(() => {
+    process.exit(1);
+  });
 });

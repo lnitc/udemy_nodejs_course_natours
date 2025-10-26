ccedc027da273958a89f36e48d0030e577aebe69
lnitc <nazebesto@yahoo.com>
2025-10-26 20:43:16 +0900

    Add logic to update current user

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,14 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+function filterObj(obj, ...allowedFields) {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+}
 
 function getAllUsers(req, res, next) {
   catchAsync(async () => {
@@ -42,10 +51,37 @@ function createUser(req, res) {
   });
 }
 
+function updateMe(req, res, next) {
+  catchAsync(async () => {
+    if (req.body.password || req.body.passwordConfirm) {
+      return next(
+        new AppError(
+          'This route is not for password updates. Please use /updatePassword.',
+          400,
+        ),
+      );
+    }
+
+    const filteredBody = filterObj(req.body, 'name', 'email'); //only include allowed fields
+    const user = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+      new: true,
+      runValidators: true,
+    });
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        user,
+      },
+    });
+  })(req, res, next);
+}
+
 module.exports = {
   getAllUsers,
   getUser,
   updateUser,
   deleteUser,
   createUser,
+  updateMe,
 };

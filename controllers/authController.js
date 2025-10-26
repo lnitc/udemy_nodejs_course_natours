cada58ecb79299e52be2990459e5342180e88237
lnitc <nazebesto@yahoo.com>
2025-10-26 22:09:38 +0900

    Implement jwt cookies

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,6 +13,18 @@ function signToken(id) {
 
 function createSendToken(user, statusCode, res) {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000,
+    ),
+    httpOnly: true, //to prevent browser from accessing the cookie(XSS attacks etc.)
+  };
+
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+  user.password = undefined; //remove password from response
+
   res.status(statusCode).json({ status: 'success', token, data: { user } });
 }
 
